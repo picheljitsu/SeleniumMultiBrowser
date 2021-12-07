9ae7f8991f9b9ea11c59eaad7b8ca75efd55e81a
unknown <pichelmayer@gmail.com>
2021-12-07 09:27:20 -0600

    Updated README.md

--- test.py
+++ test.py
@@ -2,7 +2,9 @@ import SeleniumMultiBrowser
 a = SeleniumMultiBrowser.SeleniumMultiBrowser(config_file='config.json')
 print(a.driver_path)
 
-
+#Specifies a list of user-agents to use.  Caution on having too many variances of browsers.  Remote
+#web servers may render web content different based on the User-Agent, which could break searching/
+#scraping and require a seperate parsing logic for each browser type.
 user_agents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
     "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2919.83 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2866.71 Safari/537.36",
@@ -15,11 +17,16 @@ user_agents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KH
 print("[*] Loading User Agents")
 a.user_agents = user_agents
 
+#Set the user_agents property of the class instance
 print(a.user_agents)
 
 #This will automatically launch a browser for each of the user-agents specified
 a.get_url(target_url='https://www.linkedin.com')
 
+
+#The class will randomly select a new browser for each .get_url() method call
+#that browser will be set as the class instances a.active_browser property
+#So each .get_url() call will automatically rotate the active browser.
 if a.set_element_id('session_key'):
     print("[*] Got login box.")
     a.send_keys(a.username)
@@ -33,17 +40,13 @@ if not a.login():
     print("[-] Failed to login.")
     exit()
     
-#Get's list of all Microsoft employees
+#This URL returns a list of all Microsoft employees.  
 a.get_url('https://www.linkedin.com/search/results/people/?currentCompany=%5B%221035%22%2C%2210073178%22%2C%223238203%22%2C%223178875%22%2C%221418841%22%2C%2219537%22%2C%223290211%22%2C%2218612750%22%2C%2211206713%22%2C%221148098%22%2C%221889423%22%2C%22164951%22%2C%223641570%22%2C%22165397%22%2C%225097047%22%2C%221386954%22%2C%22263515%22%2C%2210957831%22%2C%22589037%22%2C%22692068%22%2C%222270931%22%2C%223763403%22%2C%2230203%22%2C%222446424%22%2C%2218086638%22%5D&origin=COMPANY_PAGE_CANNED_SEARCH')
+
+#This code is incomplete, but you could essesntially parse the pages for the names
 a.get_elements_class('app-aware-link')
 a.set_elements_class('app-aware-link')
 listobjects = a.active_element
 for obj in listobjects:
     print(obj.get_property('lastElementChild').get_property('innerText'))
 
-    
-# spans = a.browsers[0].find_elements_by_tag_name('span')
-# for i in range(0, len(spans)):
-#     if 'View' in spans[i].text:
-#         print("{} {}".format(i, spans[i-1].text))
-#a.browsers[0].find_elements_by_class_name('app-aware-link')[1].get_property('lastElementChild').get_property('innerText')
